7fc9cd063cc411297fb71a078f68b2d9f8c35679
Richard Hess <eswat2@gmail.com>
2021-12-01 07:41:34 -0800

    refactor spinner into a utility function

--- src/components/proto-autos.jsx
+++ src/components/proto-autos.jsx
@@ -3,7 +3,6 @@ import { Eswat2Io, ToolBar, DealerView } from '.';
 import { actions, state, tw } from '../utils';
 
 const TW_VERSION = '2.2.19';
-const LOADING = 'loading...';
 
 const TwLabel = version => {
   return (
@@ -15,6 +14,36 @@ const TwLabel = version => {
 
 const Footer = () => {
   return <div class="flex flex-row mt-2 mb-4 mr-2">{TwLabel(TW_VERSION)}</div>;
+};
+
+const Spinner = (label = 'loading...') => {
+  return (
+    <svg
+      class="animate-spin h-24px w-24px text-clrs-navy"
+      fill="none"
+      viewBox="0 0 24 24"
+      role="img"
+      aria-label="title"
+    >
+      <title>{label}</title>
+      <g>
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </g>
+      <path d="M0 0h24v24H0z" fill="none" />
+    </svg>
+  );
 };
 
 const ProtoAutos = props => {
@@ -35,36 +64,7 @@ const ProtoAutos = props => {
         Auto Dealers
       </h1>
       <ToolBar />
-      <Show
-        when={!state.loading}
-        fallback={
-          <svg
-            class="animate-spin h-24px w-24px text-clrs-navy"
-            fill="none"
-            viewBox="0 0 24 24"
-            role="img"
-            aria-label="title"
-          >
-            <title>{LOADING}</title>
-            <g>
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              ></circle>
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </g>
-            <path d="M0 0h24v24H0z" fill="none" />
-          </svg>
-        }
-      >
+      <Show when={!state.loading} fallback={Spinner()}>
         <hr
           class={tw(
             'ml-0 mr-0 mt-4 mb-4',
