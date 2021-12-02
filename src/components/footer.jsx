7ea3e47a8b33df2ea1e031e114bd3f80c864d6d4
Richard Hess <eswat2@gmail.com>
2021-12-02 11:19:27 -0800

    add footer & spinner as simple components

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+const TW_VERSION = '2.2.19';
+
+const TwLabel = version => {
+  return (
+    <label class="ml-auto align-top text-xs italic text-clrs-slate4">
+      Tailwind {version}
+    </label>
+  );
+};
+
+const Footer = props => {
+  return <div class="flex flex-row mt-2 mb-4 mr-2">{TwLabel(TW_VERSION)}</div>;
+};
+
+export { Footer };
+export default Footer;
--- src/components/proto-autos.jsx
+++ src/components/proto-autos.jsx
@@ -1,50 +1,6 @@
 import { onMount, Show } from 'solid-js';
-import { Eswat2Io, ToolBar, DealerView } from '.';
+import { Eswat2Io, Spinner, ToolBar, Footer, DealerView } from '.';
 import { actions, state, tw } from '../utils';
-
-const TW_VERSION = '2.2.19';
-
-const TwLabel = version => {
-  return (
-    <label class="ml-auto align-top text-xs italic text-clrs-slate4">
-      Tailwind {version}
-    </label>
-  );
-};
-
-const Footer = () => {
-  return <div class="flex flex-row mt-2 mb-4 mr-2">{TwLabel(TW_VERSION)}</div>;
-};
-
-const Spinner = (label = 'loading...') => {
-  return (
-    <svg
-      class="animate-spin h-24px w-24px text-clrs-navy"
-      fill="none"
-      viewBox="0 0 24 24"
-      role="img"
-      aria-label="title"
-    >
-      <title>{label}</title>
-      <g>
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </g>
-      <path d="M0 0h24v24H0z" fill="none" />
-    </svg>
-  );
-};
 
 const ProtoAutos = props => {
   onMount(async () => {
@@ -64,7 +20,7 @@ const ProtoAutos = props => {
         Auto Dealers
       </h1>
       <ToolBar />
-      <Show when={!state.loading} fallback={Spinner()}>
+      <Show when={!state.loading} fallback={<Spinner />}>
         <hr
           class={tw(
             'ml-0 mr-0 mt-4 mb-4',
@@ -74,7 +30,7 @@ const ProtoAutos = props => {
         />
       </Show>
       <DealerView />
-      {Footer()}
+      <Footer />
     </main>
   );
 };
