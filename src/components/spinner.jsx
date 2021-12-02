266b60243d6e9cc2047bb6b48593c2df43b3c247
Richard Hess <eswat2@gmail.com>
2021-12-02 11:30:39 -0800

    refactor spinner

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -1,8 +1,9 @@
 import { tw } from '../utils';
 
 const Spinner = props => {
+  const hex = props.hex || 'currentColor';
   const klass = props.class || '';
-  const label = props.label || 'loading';
+  const label = props.label || 'loading...';
   const size = props.size || 24;
 
   return (
@@ -12,7 +13,6 @@ const Spinner = props => {
       height={size}
       fill="none"
       viewBox="0 0 24 24"
-      role="img"
       aria-label="title"
     >
       <title>{label}</title>
@@ -22,12 +22,12 @@ const Spinner = props => {
           cx="12"
           cy="12"
           r="10"
-          stroke="currentColor"
+          stroke={hex}
           stroke-width="4"
         ></circle>
         <path
           class="opacity-75"
-          fill="currentColor"
+          fill={hex}
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </g>
